6091925ec04bc0bd83e0678e9d539637328dddc1
Giuseppe Fiore <giuseppe100891fiore@gmail.com>
2026-02-05 11:56:20 +0100

    complete snack-3

--- snack-3/main.js
+++ snack-3/main.js
@@ -1,3 +1,12 @@
 // snack 3
 // Il software deve chiedere per 10 volte all’utente di inserire un numero.
 // Il programma stampa la somma di tutti i numeri inseriti.
+
+let somma = 0;
+
+for (let i = 0; i < 10; i++) {
+    const numero = Number(prompt(`Inserisci il numero ${i + 1}:`));
+    somma += numero;
+}
+
+console.log("La somma totale è: " + somma);
