70d8d1ab92963c4abb4fb1cd2c708b1f22b5b2f5
Giuseppe Fiore <giuseppe100891fiore@gmail.com>
2026-02-05 10:30:35 +0100

    first commit

---
+++ snack-1/main.js
@@ -0,0 +1,6 @@
+// snack 1
+// L’utente inserisce due numeri in successione, con due prompt.
+// Il software stampa il maggiore.
+
+const a = Number(prompt("Inserisci il primo numero:"));
+const b = Number(prompt("Inserisci il secondo numero:"));
